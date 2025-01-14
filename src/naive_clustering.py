4c0e3b1cf460ebdef9b8777605259941501c7b28
Jan <hoeckendorff@cs.uni-koeln.de>
2025-01-14 17:02:12 +0100

    progress for naive clustering

--- src/naive_clustering.py
+++ src/naive_clustering.py
@@ -3,6 +3,7 @@ import cartopy.crs as ccrs
 import matplotlib.pyplot as plt
 import math
 import numpy as np
+from tqdm import tqdm
 
 def distance_function(lat1, long1, timeseries1, lat2, long2, timeseries2):
     """
@@ -41,43 +42,59 @@ center_lons = [156, -133, -67 , -35, 147, -2]
 center_lats = [25, 10, 29, 60, -55, -43]
 colors = ['red','yellow','green','cyan','blue','purple']
 
-plt.scatter(center_lons, center_lats, color = colors, marker = 'x')
-path = "../Data/Copernicus/sea_level_anomaly_data.nc"
+
+path = "../Data/Sea_Level_Data/sea_level_anomaly_data.nc"
 data = xr.open_dataset(path)
+sea_level = data['sla']
+#sea_level.isel(time = 0).plot(ax=ax, transform=ccrs.PlateCarree(), cmap='jet', add_colorbar=True)
 
-all_lats = data['latitude'].values
-all_lons = data['longitude'].values
 
-#print(data['sla'].loc[:,all_lats[100], all_lons[100]].values)
+valid_points = ~sea_level.isnull().any(dim='time')
+print(valid_points)
+#filtered_sea_level = sea_level.where(valid_points,drop=True)
+
+all_lats = sea_level['latitude'].values
+all_lons = sea_level['longitude'].values
 #get nearest entities for hardcoded centers
 
 centers = []
-for i in range(len(center_lats)):
+for i in tqdm(range(len(center_lats))):
 
-    centers.append(data['sla'].sel(latitude = center_lats[i], longitude = center_lons[i], method = 'nearest'))
-    center_lons[i] = float(centers[i]['longitude'])
-    center_lats[i] = float(centers[i]['latitude'])
- 
+    tmp = sea_level.sel(latitude = center_lats[i], longitude = center_lons[i], method = 'nearest')
+    centers.append(tmp.values)
+    center_lons[i] = float(tmp['longitude'])
+    center_lats[i] = float(tmp['latitude'])
+    
+print(centers)
 
-assignment = [ [0]*len(all_lats) for i  in range(len(all_lons))]
-
-time_series = data['sla'].loc[:,all_lats[0], all_lons[0]].values
-
-print(distance_function(all_lats[0],all_lons[0],time_series,center_lats[0],center_lons[0],centers[0].values))
-#for i,lat in enumerate(all_lats):
-#    for j,lon in enumerate(all_lons):
-#        time_series = data['sla'].loc[:,lat, lon].values
-#        min_dist = math.inf
-#        for k in range(len(centers)):
-#            c = centers[k].values
-#            tmp = distance_function(lat,lon,time_series,)
-
-       #print(distance_function(lat,lon))
-#       print("test")
+assignment = []   
+lats = []
+lons = []
 
 
+clustering_variable = np.zeros((sea_level.latitude.size, sea_level.longitude.size))
+clustering_var_da = xr.DataArray(data=clustering_variable[:, :],
+                                dims=['latitude', 'longitude'],
+                                coords={ 'latitude': sea_level.latitude, 'longitude': sea_level.longitude })
 
-#print(centers[0])
-#print(centers[0].values)
+#compute clustering
+for i,lat in tqdm(enumerate(all_lats[:300])):
+    for j,lon in enumerate(all_lons):
+        if valid_points[i][j]:
+            time_series = sea_level.loc[:,lat, lon].values
+            min_dist = math.inf
+            #find closest center
+            for k in range(len(centers)):
+                c = centers[k]
+                tmp = distance_function(lat,lon,time_series,center_lats[k],center_lons[k],c)
+                if tmp < min_dist:
+                    tmp = min_dist
+                    assignment = k
+                clustering_var_da[i,j] = assignment
+        else:
+            clustering_var_da[i,j] = 'nan'
+clustering_var_da.plot()
+plt.show()
+
 
 
